b9a40bd378efaffdc86c46711fcc682cdd193a13
Temur <77517397+TemurBahri@users.noreply.github.com>
2022-09-18 23:30:10 +0300

    Java

--- JavaKampÖdev22/src/variables.java
+++ JavaKampÖdev22/src/variables.java
@@ -1,9 +1,14 @@
 
 public class variables {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        int ogrenciSayisi = 12;
+        String mesaj = "Öðrenci Sayýsý : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öðrenci sayým : " + ogrenciSayisi);
+        System.out.println("Öðrenci sayým : " + ogrenciSayisi);
+        System.out.println("Öðrenci sayým : " + ogrenciSayisi);
 
-	}
-
+    }
 }

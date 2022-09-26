a44c5ce0ef61110fbaa5e084089e0c1ee799e4f1
Temur <77517397+TemurBahri@users.noreply.github.com>
2022-09-26 11:18:46 +0300

    HomeWork Continue ........

--- JavaKampÖdev22/src/Classes/Main.java
+++ JavaKampÖdev22/src/Classes/Main.java
@@ -7,6 +7,17 @@ public class Main  {
 		customerManager.Add();
 		customerManager.Remove();
 		customerManager.Update();
+		
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi2 = sayi1;
+		sayi1 = 30;
+		System.out.println(sayi2);
+		
+		int[] sayilar1 = new int[] {1,3,4};
+		int[] sayilar2 = new int[] {5,6,4};
+		sayilar2[0] = 10;
+		System.out.println(sayilar2[0]);
 	}
 
 }
